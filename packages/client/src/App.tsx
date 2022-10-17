39c70ebe975a1f8a1de94e44ba19e3d6cd06ac04
Jasper Hwong Yu Chen <jasperhwong0077@gmail.com>
2022-10-17 23:44:17 +0800

    create form for user and message input and run prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,39 +1,67 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { trpc } from "./trpc"
+import { trpc } from "./trpc";
 
 import "./index.scss";
 
 const queryClient = new QueryClient();
 
 const AppContent = () => {
+  const [user, setUser] = React.useState<string>();
+  const [message, setMessage] = React.useState<string>();
   const hello = trpc.useQuery(["hello", "Jasper"]);
   const response = trpc.useQuery(["getMessages"]);
 
   const addMessage = trpc.useMutation("addMessage");
   const handleAddMessage = React.useCallback(() => {
-    addMessage.mutate({
-      message: "Hello world",
-      user: "Jasper",
-    });
-  }, [])
+    addMessage.mutate(
+      {
+        message: "Hello world",
+        user: "Jasper",
+      },
+      {
+        onSuccess: () => {
+          queryClient.invalidateQueries(["getMessages"]);
+        },
+      }
+    );
+  }, []);
   const { data: messages } = response;
   return (
-    <div className="mt-10 text-3xl mx-auto max-w-6xl flex-row space-x-2">
-      <div className="flex flex-row items-center space-x-10">
+    <div className="mx-auto mt-10 flex-row space-x-2 text-3xl sm:max-w-2xl">
+      <div className="grid grid-cols-2 items-center space-x-10">
         <h1>{hello.data}</h1>
         <button
           type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          className="focus:outline-none mr-2 mb-2 w-20 place-self-end rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           onClick={handleAddMessage}
-        >Add
+        >
+          Add
         </button>
       </div>
       <br />
-      {messages?.map((msg) => (<p>
-        { msg.message }
-      </p>))}
+      {messages?.map((msg) => (
+        <p>{msg.message}</p>
+      ))}
+      <div className="mx-auto mt-10 grid grid-cols-1 gap-4">
+        <input
+          type="text"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+          className="w-full rounded-lg border-2 border-gray-300 p-5"
+          placeholder="User"
+          aria-label="user"
+        />
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          className="w-full rounded-lg border-2 border-gray-300 p-5"
+          placeholder="Message"
+          aria-label="message"
+        />
+      </div>
     </div>
   );
 };
@@ -50,6 +78,6 @@ const App = () => {
         <AppContent />
       </QueryClientProvider>
     </trpc.Provider>
-  )
-}
+  );
+};
 ReactDOM.render(<App />, document.getElementById("app"));
